f2cb3727985b8806e3e98cdd9f4bada97e795044
Pierre-Elie Fauché <pierre-elie@withdouble.com>
2024-04-02 19:58:10 -0400

    🏷 Library typings

--- src/browsers/types.d.ts
+++ src/browsers/types.d.ts
@@ -1,8 +1,50 @@
 import { ComponentProps, FunctionComponent } from 'react'
-import { FsItem } from '../file-system'
+import { FsDir, FsFile } from '../file-system'
 
 type BrowserProps = ComponentProps<'div'> & {
-  rootDir: FsItem
+  rootDir: FsDir
 }
 
 type Browser = FunctionComponent<BrowserProps>
+
+export type Library<G = unknown, B = unknown, V = unknown, S = unknown> = {
+  groups: Array<
+    G & {
+      key: string
+      name?: string | null | undefined
+      isNamable: boolean
+      isEmpty: boolean
+
+      banks: Array<
+        B & {
+          key: string
+          name?: string | null | undefined
+          isNamable: boolean
+          isEmpty: boolean
+
+          voices: Array<
+            V & {
+              key: string
+              name?: string | null | undefined
+              isNamable: boolean
+              isEmpty: boolean
+              maxSamplesCount: number
+
+              samples: Array<
+                S & {
+                  key: string
+                  fsFile: FsFile | undefined | null
+                }
+              >
+            }
+          >
+        }
+      >
+    }
+  >
+}
+
+export type Group<L extends Library> = Library['groups'][number]
+export type Bank<L extends Library> = Group<L>['banks'][number]
+export type Voice<L extends Library> = Bank<L>['voices'][number]
+export type Sample<L extends Library> = Voice<L>['samples'][number]
